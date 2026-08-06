bf4616fe79f390eecae294e081c912a8a23ecc93
facilitefacile62-alt <contact@facilite.fr>
2026-08-06 14:15:51 +0000

    fix(nav): correction de la hauteur de la modale de notifications qui se rétractait à 0px

--- .tmp_triggers2.sql
+++
@@ -1,5 +0,0 @@
-SELECT tgname FROM pg_trigger t
-JOIN pg_class c ON c.oid = t.tgrelid
-JOIN pg_namespace n ON n.oid = c.relnamespace
-WHERE NOT t.tgisinternal AND n.nspname = 'auth' AND c.relname = 'users'
-ORDER BY tgname;
